cc6df3df4e571c5fc571f7fa77863f220392afce
hechong <chong@intrii.com>
2024-10-16 11:25:39 +0800

    主应用整合nextjs mutile-zone特性

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const { SUB_MODULE_URL, WEB_URL } = process.env;
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/sub-module",
+        destination: `${SUB_MODULE_URL}/sub-module`,
+      },
+      {
+        source: "/sub-module/:path+",
+        destination: `${SUB_MODULE_URL}/sub-module/:path+`,
+      },
+      {
+        source: "/sub-module-static/_next/:path+",
+        destination: `${SUB_MODULE_URL}/sub-module-static/_next/:path+`,
+      },
+      {
+        source: "/web",
+        destination: `${WEB_URL}/web`,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
